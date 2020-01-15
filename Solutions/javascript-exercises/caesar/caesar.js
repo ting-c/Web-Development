664991d38805daeb96301c5e7bf2e5d7becef555
ting-c <ting.cheong31@gmail.com>
2020-01-15 21:23:17 +0000

    update javascript-exercises

--- Solutions/javascript-exercises/caesar/caesar.js
+++ Solutions/javascript-exercises/caesar/caesar.js
@@ -1,5 +1,42 @@
-const caesar = function() {
+const caesar = function(string, shift) {
+  shiftedWord = ''
+  for (i=0; i<string.length; i++){
+    code = string.charCodeAt(i);
+    if (code <= 64){
+      char = String.fromCharCode(code);
+      shiftedWord += char;
+      continue;
+    }
+      else if (code >= 91 & code <= 96) {
+        char = String.fromCharCode(code);
+        shiftedWord += char;
+        continue;
+      }
+      else if (code >= 123) {
+      char = String.fromCharCode(code);
+      shiftedWord += char;
+      continue;
+      }
+    if (shift > 26){
+      shift %= 26;
+    }
+    else if (shift < 0){
+      shift = 26 + shift;
+    }
+    codeShift = code + shift;
 
+    if (codeShift >= 91 & code <= 90){
+      codeShift -= 26;
+    }
+    else if (codeShift >= 123 & code <= 122){
+      codeShift -= 26;
+    }
+
+    char = String.fromCharCode(codeShift);
+    shiftedWord += char
+  }
+
+  return shiftedWord
 }
 
 module.exports = caesar
--- Solutions/javascript-exercises/reverseString/reverseString.js
+++ Solutions/javascript-exercises/reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+  splitString = string.split("");
+  newString = []
+  for (let i=0; i < string.length; i++){
+    character = splitString.pop();
+    newString.push(character);
+  }
+  return newString.join('')
 }
 
 module.exports = reverseString
