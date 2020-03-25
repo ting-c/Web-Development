5be6558dacff78772fb4e77ac6e0e4c293b06a86
ting-c <ting.cheong31@gmail.com>
2020-03-25 16:56:41 +0000

    added projects solutions

---
+++ Solutions/dog_project/lib/dog.rb
@@ -0,0 +1,38 @@
+class Dog
+    def initialize(name, breed, age, bark, favorite_foods)
+        @name = name
+        @breed = breed
+        @age = age
+        @bark = bark
+        @favorite_foods = favorite_foods
+    end
+
+    def name
+        @name
+    end
+
+    def breed
+        @breed
+    end
+
+    def age
+        @age
+    end
+
+    def age=num
+        @age = num
+    end
+
+    def bark
+        return @bark.upcase if @age > 3
+        @bark.downcase
+    end
+
+    def favorite_foods
+        @favorite_foods
+    end
+
+       def favorite_food?(food)
+        @favorite_foods.include?(food.capitalize)
+    end
+end
